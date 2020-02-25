59254fa9e94cdde6678a151a98565eb16ceb1327
root <root@kaushik>
2020-02-25 09:28:26 +0000

    changes in welcome slogan

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Jenkins World!'))
+app.get('/', (req, res) => res.send('Hello Wel Come to  Jenkins World! We have created CI/CD in Nodejs with jenkins '))
 
 var server = app.listen(13000, () => {
     console.log("Listening on port " + server.address().port + "...");
