3e3fe6b0c9b11d7d74d72c501d0c94090911dbcd
root <root@kaushik>
2020-02-26 07:23:18 +0000

    edit slogan

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello WELCOME TO JENKINS WORLD !!!!!!!!!!!!!!! THIS IS CI/CD CONFIGURE WITH NODEJS PROJCET IN JENKINS WITH AUTO DEPLOYMENT ON SERVER '))
+app.get('/', (req, res) => res.send('HELLO ..... WELCOME  TO  JENKINS  WORLD !!!!!!!!!!!!!!! THIS    IS   CI/CD   CONFIGURE  WITH  NODEJS  PROJCET  IN   JENKINS   WITH   AUTO  DEPLOYMENT  ON  SERVER '))
 
 var server = app.listen(13000, () => {
     console.log("Listening on port " + server.address().port + "...");
